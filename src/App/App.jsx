4f8de61d38eb408899300da8605be0efe9b375bb
John Doe <dev@dev.com>
2023-09-29 21:11:43 +0200

    Add lazy loading and chunk code by route

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,45 +1,50 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "root/_components.jsx";
-import { Product } from "product/_components.jsx";
-import { Pricing } from "pricing/_components.jsx";
 import {
-  AppLayout,
   City,
   CityList,
   CountryList,
   Form,
   ProtectedRoute,
 } from "app/_components.jsx";
-import { Login } from "login/_components.jsx";
-import { PageNotFound } from "utilities/_components.jsx";
+import { SpinnerFullPage } from "utilities/_components.jsx";
 import { AuthProvider, CitiesProvider } from "contexts/_components.jsx";
+
+const Home = lazy(() => import("root/Home/Home.jsx"));
+const Product = lazy(() => import("product/Product/Product.jsx"));
+const Pricing = lazy(() => import("pricing/Pricing/Pricing.jsx"));
+const Login = lazy(() => import("login/Login/Login.jsx"));
+const AppLayout = lazy(() => import("app/AppLayout/AppLayout.jsx"));
+const PageNotFound = lazy(() => import("utilities/PageNotFound.jsx"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
--- src/Utilities/_components.jsx
+++ src/Utilities/_components.jsx
@@ -4,5 +4,14 @@ import Logo from "./Logo.jsx";
 import Spinner from "./Spinner.jsx";
 import Message from "./Message.jsx";
 import Button from "./Button.jsx";
+import SpinnerFullPage from "./SpinnerFullPage.jsx";
 
-export { PageNotFound, PageNav, Logo, Spinner, Message, Button };
+export {
+  PageNotFound,
+  PageNav,
+  Logo,
+  Spinner,
+  Message,
+  Button,
+  SpinnerFullPage,
+};
